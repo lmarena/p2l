a905fa5ea94a75fdf157d73e27bd3c63ac1ebeb1
Evan Frick <evanfrick@Evans-MacBook-Pro-2.local>
2025-05-09 12:31:07 -0700

    a

---
+++ fast_lambda_setup.sh
@@ -0,0 +1,18 @@
+sudo apt-get update -y
+sudo apt-get install tmux -y
+sudo apt-get install python3-dev -y
+
+sudo apt-get install tmux libaio-dev libopenmpi-dev python3-mpi4py -y
+
+curl -LsSf https://astral.sh/uv/install.sh | sh
+
+source $HOME/.local/bin/env
+
+uv venv .env --python 3.10
+
+source .env/bin/activate
+
+uv pip install wheel packaging
+
+uv pip install -r train_requirements.txt
+uv pip install flash-attn==2.5.9.post1 --no-build-isolation
